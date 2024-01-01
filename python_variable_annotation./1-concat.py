5293cc6381313b5d849acd8325ee84bf4355fa7a
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-01 20:28:53 +0100

    addec task 1

---
+++ python_variable_annotation./1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a module with a function that concats two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """a function that concats two strings"""
+    return str1 + str2
