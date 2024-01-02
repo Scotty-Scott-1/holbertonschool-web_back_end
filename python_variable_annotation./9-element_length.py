db46c2af35cbfb3c451936b1d2ff309dc2fadf0d
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-02 15:03:06 +0100

    Added task 9

---
+++ python_variable_annotation./9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""a module with a function that takes list or tuple containing sequence types
+and returns a list of tuples(element 1: the sequance, element 2: len )"""
+
+
+from typing import Tuple, Sequence, Iterable, List
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """a module with a function
+    that takes list or tuple containing sequence types
+    and returns a list of tuples(element 1: the sequance, element 2: len )"""
+    return [(i, len(i)) for i in lst]
