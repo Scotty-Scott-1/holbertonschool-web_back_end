d34804c7a011e51f75740c4a1b952b031e6bbd36
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-02 10:29:10 +0100

    Added task 2

---
+++ python_variable_annotation./2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""a module with a function that returns the floor of arg n"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """a function that returns the floor of n"""
+    return math.floor(n)
