2cfbd30ffb1ee39a2f6297cfa4fbe7db3380e49c
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-02 14:13:54 +0100

    added task 7

---
+++ python_variable_annotation./7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""a module with a function that returns a tuple"""
+
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns a tuple where first element is arg: k
+    and second arg: v squared"""
+
+    second: float = v ** 2
+    return k, second
