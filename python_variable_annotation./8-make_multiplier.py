bf4f6c9d2086cf2b0b7bfe3681fec4b1ba5abbf0
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-02 14:35:01 +0100

    added task 8

---
+++ python_variable_annotation./8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""a module with a function that returns a functione"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """a function that returns a function"""
+    def func(num: float) -> float:
+        """returns an arg * multiplier"""
+        return num * multiplier
+
+    return func
