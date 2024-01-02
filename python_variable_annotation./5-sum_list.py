349bd16a7147698da607dead758b7badf7af307e
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-02 13:12:55 +0100

    added task 5

---
+++ python_variable_annotation./5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a module with a function that returns the sum of a list"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of a list"""
+    return sum(input_list)
