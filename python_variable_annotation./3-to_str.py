91a930b336d59cc7192e1b44ceeb96605eebd8f1
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-02 10:34:04 +0100

    added task 3

---
+++ python_variable_annotation./3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a module with a function that returns the floor of arg n"""
+
+
+def to_str(n: float) -> str:
+    """a function that returns the floor of n"""
+    return str(n)
