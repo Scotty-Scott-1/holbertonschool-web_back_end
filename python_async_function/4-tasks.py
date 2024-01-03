9df2f8d1ebbbc57c640faed5500fb157e17db8f2
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-04 00:14:05 +0100

    added task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""import wait random from task 0
+new async routine called: wait_n
+takes 2 args of type int. n and max_delay
+call wait_random n times with max_delay
+return a list of delays (floats)
+"""
+
+
+from asyncio import gather, sleep
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """takes 2 args of type int. n and max_delay
+    call wait_random n times with max_delay
+    return a list of delays (floats)"""
+
+    async def delay() -> None:
+        """wait for the required time and append to list"""
+        delay = await task_wait_random(max_delay)
+        await sleep(delay)
+        result_list.append(delay)
+
+    result_list: List[float] = []
+
+    my_list = [delay() for _ in range(n)]
+
+    await gather(*my_list)
+
+    return result_list
