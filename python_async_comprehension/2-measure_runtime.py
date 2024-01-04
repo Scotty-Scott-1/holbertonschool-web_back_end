61c9ce5906ed882d2cf29afd0f49451a2f50094c
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-04 13:05:41 +0100

    added task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+execute async_comprehension four times in parallel using asyncio.gather.
+measure_runtime should measure the total runtime and return it.
+
+"""
+
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    execute async_comprehension four times in parallel using asyncio.gather.
+    measure_runtime should measure the total runtime and return it.
+
+    """
+    start = time.time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end = time.time()
+
+    result = end - start
+    return result
