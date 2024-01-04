e7727db38ed711561b6fcde555b58e094e03cf14
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-04 11:41:36 +0100

    Added new dir python_async_comprehension added readme added task 0 added gitignore

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""an async generato that yeilds a random numberr"""
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """in a loop of 10, wait one second, yeild a random number"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random()
