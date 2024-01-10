ac795174cd891752707426a30d2bfe789347adad
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-10 15:35:30 -0800

    added task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A module that updated docuemnts"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Insert a document"""
+
+    result = mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topcis": topics}}
+    )
+    return result
