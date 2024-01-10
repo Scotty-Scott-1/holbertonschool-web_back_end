69f68155121e90f4fb352663a17e0062967e0966
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-10 15:58:13 -0800

    added task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A module that returns specific schools"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Find only the schools that have a specific topic"""
+
+    result = mongo_collection.find(
+            {"topics": topic}
+    )
+    return result
