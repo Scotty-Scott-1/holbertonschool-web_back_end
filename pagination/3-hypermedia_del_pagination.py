c0418e69da83fc7645bb4eaa8536ef19974f4346
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-12 01:22:47 +0100

    added task 3

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        assert isinstance(index, int) and index >= 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        indexed_data = self.indexed_dataset()
+        total_items = len(self.dataset())
+        start = 0 if index is None else index
+        end = min(start + page_size - 1, total_items - 1)
+        next_index = min(end + 1, total_items) if end < total_items else None
+        data_page = []
+        current_index = start
+
+        while len(data_page) < page_size and current_index < total_items:
+            if current_index in indexed_data:
+                data_page.append(indexed_data[current_index])
+            current_index += 1
+
+        result = {
+            "index": start,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data_page
+        }
+
+        return result
