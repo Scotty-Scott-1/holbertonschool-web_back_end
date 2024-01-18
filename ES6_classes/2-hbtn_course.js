8108f66aee84babd2c828257a6bd27d143f07f9d
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-18 23:18:25 +0100

    Added task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,61 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw TypeError('Length must be a number');
+    }
+
+    if (Array.isArray(students)) {
+      if (students.every((item) => typeof item === 'string')) {
+        this._students = students;
+      } else {
+        throw TypeError('Students must be an array of strings');
+      }
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      if (students.every((item) => typeof item === 'string')) {
+        this._students = students;
+      } else {
+        throw TypeError('Students must be an array of strings');
+      }
+    }
+  }
+}
