8f7404ebecd47c05c36b5d28f3d0dc480c532671
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-19 00:25:45 +0100

    added task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing extends Currency {
+  constructor(amount, currency) {
+    super();
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
