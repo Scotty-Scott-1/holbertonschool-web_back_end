98bc8e4c6f12277069470e77a7964cd463472b67
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-26 01:46:31 +0000

    added task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto as promise1, createUser as promise2 } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([promise1(), promise2()])
+    .then((results) => {
+      const result = `${results[0].body} ${results[1].firstName} ${results[1].lastName}`;
+      console.log(result);
+    })
+    .catch(() => new Error('Signup system offline'));
+}
