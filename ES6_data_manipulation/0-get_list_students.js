56999ba65ef6dfaf5bfd0767c9fae3b72c56a830
Scotty_Scott-1 <swilliamfr@outlook.com>
2024-01-29 11:16:51 +0100

    Added new dir ES6_data_manipulation Added task 1 0-get_list_students.js Added README

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const ob1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const ob2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const ob3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  const myArray = [ob1, ob2, ob3];
+
+  return myArray;
+}
