140a6165a217a205b80f2b489d9612e6baf6bc37
Scotty_Scott-1 <swilliamfr@outlook.com>
2024-01-30 16:04:48 +0100

    updated 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8array = new Int8Array(buffer);
-  try {
-    int8array[position] = value;
-    const dataView = new DataView(buffer);
-    return {
-      byteLength: dataView.byteLength,
-      byteOffset: int8array.byteOffset,
-      buffer,
-    };
-  } catch (error) {
+  if (position < 0 || position > length) {
     throw new Error('Position outside range');
   }
+  const buffer = new ArrayBuffer(length);
+  const int8array = new Int8Array(buffer);
+  int8array[position] = value;
+  const dataView = new DataView(buffer);
+  return dataView;
 }
