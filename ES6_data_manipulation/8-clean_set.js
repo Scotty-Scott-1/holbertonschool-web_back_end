f201e5d429b9fab9f91888a35836bd61a10e4711
Scotty_Scott-1 <swilliamfr@outlook.com>
2024-01-30 17:28:32 +0100

    added 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  let myStr = '';
+  const len = startString.length;
+  if (len === 0) {
+    return myStr;
+  }
+  for (const element of set) {
+    const sliced = element.slice(0, len);
+    if (sliced === startString) {
+      const newSlice = element.slice(len);
+      myStr += newSlice;
+      myStr += '-';
+    }
+  }
+  const myNewStr = myStr.slice(0, -1);
+  return myNewStr;
+}
